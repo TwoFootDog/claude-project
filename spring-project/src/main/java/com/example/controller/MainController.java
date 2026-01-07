0d733c3d1e733b5743df46747ba284e16cb28c3f
twofootdog <ekfrl2815@gmail.com>
2026-01-07 09:38:55 +0900

    test4

--- spring-project/src/main/java/com/example/controller/MainController.java
+++ spring-project/src/main/java/com/example/controller/MainController.java
@@ -2,7 +2,10 @@ package com.example.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
+
 @RestController
+@Log4j2
 public class MainController {
 
 }
